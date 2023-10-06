13735bc8cd1ae3614cc1ee326e4414359b54df6d
Developer-Sahariar-Reza <dev.sahariar.reza@gmail.com>
2023-10-06 14:33:49 +0600

    login page added

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,44 @@
+import loginImg from "../../assets/images/login-reg/login.svg";
+
+const Login = () => {
+  return (
+    <section className="container mx-auto my-20 grid grid-cols-2 gap-10 justify-center items-center">
+      <div>
+        <img src={loginImg} alt="" />
+      </div>
+      <div className="border p-14 rounded-lg border-[#D0D0D0]">
+        <h1 className="text-center text-3xl font-bold mb-8">Login</h1>
+
+        <form>
+          <div className="form-control w-full mx-auto mb-5">
+            <label className="label">
+              <span className="label-text font-bold">Email</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              className="input input-bordered w-full"
+            />
+          </div>
+
+          <div className="form-control w-full mx-auto mb-5">
+            <label className="label">
+              <span className="label-text font-bold">Password</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Your password"
+              className="input input-bordered w-full"
+            />
+          </div>
+
+          <button className="common-button">Login</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,6 +6,7 @@ import About from "../pages/About/About";
 import Services from "../pages/Services/Services";
 import Blog from "../pages/Blog/Blog";
 import Contact from "../pages/Contact/Contact";
+import Login from "../pages/Login/Login";
 
 export const router = createBrowserRouter([
   {
@@ -32,6 +33,10 @@ export const router = createBrowserRouter([
       {
         path: "contact",
         element: <Contact />,
+      },
+      {
+        path: "login",
+        element: <Login />,
       },
     ],
   },
